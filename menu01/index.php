2a4691406f7c733c663a1c04cff949d8a857db21
manhldvxut <51306115+manhldvxut@users.noreply.github.com>
2020-11-25 10:54:50 +0900

    test

---
+++ menu01/index.php
@@ -0,0 +1,24 @@
+<?php
+//下記のパス「demo」を変更
+include_once $_SERVER['DOCUMENT_ROOT'].'/page/pro_name/assets/inc/config.php'; ?>
+<!-- include header --> 
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/'.DIRNAME.'/assets/inc/header.php'; ?>
+<main><?php
+//下記のパス「demo」を変更
+include_once $_SERVER['DOCUMENT_ROOT'].'/page/pro_name/assets/inc/config.php'; ?>
+<!-- include header --> 
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/'.DIRNAME.'/assets/inc/header.php'; ?>
+<main>
+	<div class="container">
+        <h2> h2 - This is Menu 01 page</h2>
+        <h3> h3 - This is Menu 01 page</h3> 
+        <h4> h4 - This is Menu 01 page</h4>
+        <a href="<?php echo DIRNAME; ?>/menu01/detail/" style="display: block; font-size: 16px; font-weight: bold;">Page Menu 01 Detail click</a>
+    </div>
+</main>
+
+<!-- include footer --> 
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/'.DIRNAME.'/assets/inc/footer.php'; ?></main>
+
+<!-- include footer --> 
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/'.DIRNAME.'/assets/inc/footer.php'; ?>
---
+++ menu04/index.php
@@ -0,0 +1,23 @@
+<?php
+//下記のパス「demo」を変更
+include_once $_SERVER['DOCUMENT_ROOT'].'/page/pro_name/assets/inc/config.php'; ?>
+<!-- include header --> 
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/'.DIRNAME.'/assets/inc/header.php'; ?>
+<main><?php
+//下記のパス「demo」を変更
+include_once $_SERVER['DOCUMENT_ROOT'].'/page/pro_name/assets/inc/config.php'; ?>
+<!-- include header --> 
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/'.DIRNAME.'/assets/inc/header.php'; ?>
+<main>
+	<div class="container">
+     <h2> h2 - This is Menu 04 page</h2>
+     <h3> h3 - This is Menu 04 page</h3> 
+     <h4> h4 - This is Menu 04 page</h4>  
+    </div>
+</main>
+
+<!-- include footer --> 
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/'.DIRNAME.'/assets/inc/footer.php'; ?></main>
+
+<!-- include footer --> 
+<?php include_once $_SERVER['DOCUMENT_ROOT'].'/'.DIRNAME.'/assets/inc/footer.php'; ?>
